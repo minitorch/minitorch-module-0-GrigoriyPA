6c508c9d1fe04e49cfe8ced0890449203a1b62da
Pisarenko Grigoriy <ga_pisarenko@mail.ru>
2025-09-29 13:43:28 +0300

    Wrote first task

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -3,7 +3,7 @@
 import math
 
 # ## Task 0.1
-from typing import Callable, Iterable
+from typing import Callable, Iterable, List
 
 #
 # Implementation of a prelude of elementary functions.
@@ -32,7 +32,72 @@ from typing import Callable, Iterable
 # $f(x) = |x - y| < 1e-2$
 
 
-# TODO: Implement for Task 0.1.
+def mul(x: float, y: float) -> float:
+    return x * y
+
+
+def id(x: float) -> float:
+    return x
+
+
+def add(x: float, y: float) -> float:
+    return x + y
+
+
+def neg(x: float) -> float:
+    return -x
+
+
+def lt(x: float, y: float) -> bool:
+    return x < y
+
+
+def eq(x: float, y: float) -> bool:
+    return x == y
+
+
+def max(x: float, y: float) -> float:
+    return x if x > y else y
+
+
+def is_close(x: float, y: float) -> bool:
+    return abs(x - y) < 1e-2
+
+
+def sigmoid(x: float) -> float:
+    if x >= 0:
+        return 1 / (1 + math.exp(-x))
+
+    ex = math.exp(x)
+    return ex / (1 + ex)
+
+
+def relu(x: float) -> float:
+    return max(x, 0)
+
+
+def log(x: float) -> float:
+    return math.log(x)
+
+
+def exp(x: float) -> float:
+    return math.exp(x)
+
+
+def log_back(x: float, d: float) -> float:
+    return d / x
+
+
+def inv(x: float) -> float:
+    return 1 / x
+
+
+def inv_back(x: float, d: float) -> float:
+    return -d / (x ** 2)
+
+
+def relu_back(x: float, d: float) -> float:
+    return d if x > 0 else 0
 
 
 # ## Task 0.3
@@ -51,4 +116,37 @@ from typing import Callable, Iterable
 # - prod: take the product of lists
 
 
-# TODO: Implement for Task 0.3.
+def map(fn: Callable[[float], float], it: Iterable[float]) -> Iterable[float]:
+    """Applies fn to every element in Iterable."""
+    for x in it:
+        yield fn(x)
+
+
+def zipWith(fn: Callable[[float, float], float], it_left: Iterable[float], it_right: Iterable[float]) -> Iterable[float]:
+    """Applies fn to pairs from left Iterable and right Iterable."""
+    for x, y, in zip(it_left, it_right):
+        yield fn(x, y)
+
+
+def reduce(fn: Callable[[float, float], float], it: Iterable[float], start: float) -> float:
+    """Reduces lst with fn starting from start."""
+    result = start
+    for x in it:
+        result = fn(result, x)
+    return result
+
+
+def negList(lst: List[float]) -> List[float]:
+    return list(map(neg, lst))
+
+
+def addLists(left_lst: List[float], right_lst: List[float]) -> List[float]:
+    return list(zipWith(add, left_lst, right_lst))
+
+
+def sum(lst: List[float]) -> float:
+    return reduce(add, lst, 0)
+
+
+def prod(lst: List[float]) -> float:
+    return reduce(mul, lst, 1)
